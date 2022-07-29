90024f70c408e1ade90cf8527274e0ef05a0a272
viry <vbatten10@hotmail.com>
2022-07-29 18:03:40 -0500

    Finalizando la funcionalidad de LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -6,7 +6,20 @@ import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  // Orden en el que se definen los useEffect, será el orden en el que se ejecutan
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS);
+    };
+    obtenerLS();
+  }, []); // Con arreglo vacío se ejecuta una sola vez
   
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
+
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter(
       paciente => paciente.id !== id
