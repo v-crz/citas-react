278988852d8b968def178e8b88d6040a66108133
viry <vbatten10@hotmail.com>
2022-07-29 14:35:49 -0500

    Mostrar textos de forma condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,21 +3,33 @@ import Paciente from "./Paciente"
 Paciente
 const ListadoPacientes = ({pacientes}) => {
 
+
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {''}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
-
-      { pacientes.map((paciente) => 
-        <Paciente 
-          key={paciente.id}
-          paciente={paciente}
-        />
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+    
+          { pacientes.map((paciente) => 
+            <Paciente 
+              key={paciente.id}
+              paciente={paciente}
+            />
+          )}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+          </p>
+        </>
       )}
-
     </div>
   )
 }
