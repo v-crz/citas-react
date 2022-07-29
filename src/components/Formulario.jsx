a64efa7ccb789cf31037f740d15c8d1301d5a98b
viry <vbatten10@hotmail.com>
2022-07-29 12:03:03 -0500

    Prop {children}

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -57,7 +57,9 @@ const Formulario = ({pacientes, setPacientes}) => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
-        {error && <Error mensaje="Todos los campos son obligatorios"/>}
+        {error && <Error>
+          <p>Todos los campos son obligatorios</p>
+        </Error>}
         <div className="mb-5">
           <label htmlFor="mascota"
             className="block text-gray-700 uppercase font-bold">
