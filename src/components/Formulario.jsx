0402ce70bb3a56b3d6b309f9102ba58564fcac6b
viry <vbatten10@hotmail.com>
2022-07-28 21:40:24 -0500

    Mostrar mensaje de error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,9 +7,18 @@ const Formulario = () => {
   const [fecha, setFecha] = useState('');
   const [sintomas, setSintomas] = useState('');
 
+  const [error, setError] = useState(false);
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log('Enviando formulario')
+    
+    // Validación del formulario
+    if([nombre, propietario, email, fecha, sintomas].includes('')){
+      console.log('Hay al menos un campo vacio');
+      setError(true);
+      return;
+    }
+    setError(false);
   }
 
   return (
@@ -27,7 +36,13 @@ const Formulario = () => {
 
       <form 
         onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+        {error &&
+          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        }
         <div className="mb-5">
           <label htmlFor="mascota"
             className="block text-gray-700 uppercase font-bold">
